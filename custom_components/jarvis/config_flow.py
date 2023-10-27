7d6402be913f7015a1e9888f18421f22559f97c8
Igor Borges <igor@borges.dev>
2023-10-27 13:53:18 -0300

    OMG

--- custom_components/jarvis/config_flow.py
+++ custom_components/jarvis/config_flow.py
@@ -57,7 +57,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         if user_input is None:
             return self.async_show_form(
-                step_id="init", data_schema=STEP_USER_DATA_SCHEMA
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
             )
 
         errors = {}
@@ -71,7 +71,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title="J.A.R.V.I.S.", data=user_input)
 
         return self.async_show_form(
-            step_id="init", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
 
     @staticmethod
