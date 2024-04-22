1890c55132581ca7add0d2ddc239503ee14fcd5f
Igor Borges <igor@borges.dev>
2024-04-22 12:01:01 -0300

    Blurg

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 import logging
 import traceback
 import httpx
+from typing import Literal
 
 from homeassistant.components import conversation
 from homeassistant.config_entries import ConfigEntry
@@ -13,11 +14,16 @@ from homeassistant.const import MATCH_ALL
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv, intent
 from homeassistant.util import ulid
-from typing import Literal
 
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+
+logging.basicConfig(
+    format="%(levelname)s [%(asctime)s] %(name)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
 
 CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
@@ -47,7 +53,7 @@ class JARVISAgent(conversation.AbstractConversationAgent):
         """Initialize the agent."""
         self.hass = hass
         self.entry = entry
-        self.http_client = httpx.AsyncClient()
+        self.http_client = httpx.Client()
 
     @property
     def supported_languages(self) -> list[str] | Literal["*"]:
@@ -66,24 +72,28 @@ class JARVISAgent(conversation.AbstractConversationAgent):
         last_msg_text = None
         try:
             _LOGGER.info('FULL ROUND:')
-            response = await self.http_client.post(
+            json_request = {
+                "input": user_input.text,
+                "config": {"configurable": {"session_id": conversation_id}},
+            }
+            _LOGGER.info(json_request)
+
+            response = self.http_client.post(
                 "http://192.168.10.20:10055/invoke",
-                json={
-                    "input": user_input.text,
-                    "config": {"configurable": {"session_id": conversation_id}},
-                },
+                json=json_request,
             )
             response.raise_for_status()
 
             json_obj = response.json()
-            _LOGGER.debug(json_obj)
+            _LOGGER.info(json_obj)
+
             last_msg_text = (
                 json_obj.get("output")
                 if response.status_code == 200
                 else f"Sorry, I can't do that (got error {response.status_code})"
             )
 
-            _LOGGER.info(f"msg: {json_obj}")
+            _LOGGER.info(f"msg: {last_msg_text}")
         except Exception as err:
             intent_response = intent.IntentResponse(language=user_input.language)
             intent_response.async_set_error(
